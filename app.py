4c9874a1feff1a4dd56a2af7c1fe42631d21b853
Davi-Ferreira-Bandeira <davibandeira790@gmail.com>
2025-06-26 20:05:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ def load_data():
 
 df = load_data()
 
+month_order = ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"]
+df_f["mes_label"] = pd.Categorical(df_f["mes_label"],categories=month_order, ordered=True)
+
+
 
 # ---------- COMPONENTES REUTILIZÁVEIS ----------
 def cards_overview(df_f):
